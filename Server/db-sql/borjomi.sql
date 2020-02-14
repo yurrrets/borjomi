4fdef593d140a6b6922d00fe513d303d4057e205
yurchik <yurchick@sumix.com>
2020-02-14 15:29:13 +0200

    Update db dump

--- Server/db-sql/borjomi.sql
+++ Server/db-sql/borjomi.sql
@@ -1,13 +1,15 @@
--- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `borjomi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `borjomi`;
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: borjomi
+-- Host: localhost    Database: borjomi
 -- ------------------------------------------------------
--- Server version	5.7.28-0ubuntu0.18.04.4
+-- Server version	8.0.15
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +23,7 @@
 
 DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `account` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -42,28 +44,12 @@ INSERT INTO `account` VALUES (1,'test','test','test@test.com'),(2,'t1','t1','t1@
 UNLOCK TABLES;
 
 --
--- Table structure for table `account_token`
---
-
-DROP TABLE IF EXISTS `account_token`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `account_token` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `token` varchar(100) NOT NULL,
-  `account_id` int(11) NOT NULL,
-  `expire_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `account_rel`
 --
 
 DROP TABLE IF EXISTS `account_rel`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `account_rel` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_account_id` int(11) NOT NULL,
@@ -82,9 +68,38 @@ INSERT INTO `account_rel` VALUES (1,1,2),(2,1,3);
 /*!40000 ALTER TABLE `account_rel` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `account_token`
+--
 
+DROP TABLE IF EXISTS `account_token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `account_token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(100) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `expire_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account_token`
+--
+
+LOCK TABLES `account_token` WRITE;
+/*!40000 ALTER TABLE `account_token` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account_token` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
 
 DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `message` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` int(11) NOT NULL,
@@ -96,9 +111,25 @@ CREATE TABLE `message` (
   `valid_until` datetime DEFAULT NULL,
   `params` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message_answer`
+--
 
 DROP TABLE IF EXISTS `message_answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `message_answer` (
   `id` int(11) NOT NULL,
   `message_id` int(11) NOT NULL,
@@ -107,8 +138,16 @@ CREATE TABLE `message_answer` (
   `notes` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `message_answer`
+--
 
+LOCK TABLES `message_answer` WRITE;
+/*!40000 ALTER TABLE `message_answer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message_answer` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -119,4 +158,4 @@ CREATE TABLE `message_answer` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-08 23:43:41
+-- Dump completed on 2020-02-14 15:27:29
